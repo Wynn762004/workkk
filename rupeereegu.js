9fbd113aaaf75ffd1e6294f6e06ac719d50076b4
Wynn762004 <zazaopama777@gmail.com>
2024-06-13 09:58:26 +0700

    Add files via upload

---
+++ rupeereegu.js
@@ -0,0 +1,6 @@
+function rupeereepu() {
+    let number1 = document.getElementById("number1");
+    let result = document.getElementById("result");
+    let sum = parseInt(number1.value) * 0.44 ;
+    result.innerHTML = sum;
+} 
